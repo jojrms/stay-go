81ac740a32ff530b6052d0a0d15bb6c0a8fd60e5
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-05-12 15:35:40 -0400

    💣 CODIFICAÇÃO - Finalização da section dois e três. Construção da quarta.

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -84,15 +84,45 @@ const LandingPage = () => {
             <section className='sectionTwo'>
                 <aside>
                     <article>
-                        <span>About Us</span>
+                        <span className='spanAboutUs'>About Us</span>
                         <h1>The best hotel <br/> provider for you</h1>    
                     </article>
                     <article>
-                        
+                        <h2>StayGo is an application based online platform that provides the <br/> best hotel booking service with the best destinations in Indonesia</h2>
+                        <div>
+                            <span>
+                                <h3>Hotels & Resorts</h3>
+                                <h3>672+</h3>
+                            </span>
+                            <span>
+                                <h3>Booking partnes</h3>
+                                <h3>92+</h3>
+                            </span>
+                            <span>
+                                <h3>Customer</h3>
+                                <h3>50.125+</h3>
+                            </span>
+                        </div>
                     </article>
+                </aside>
+                <aside>
+                    <img src='https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/>
+                    <img src='https://images.unsplash.com/photo-1584132967334-10e028bd69f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/>
+                    <img src='https://images.unsplash.com/photo-1583522862616-c7c405b9e0ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/>
+                    <img src='https://images.unsplash.com/photo-1583522862857-bd9f6d34a236?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/>
+                    <img src='https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'/>
                 </aside>
                 
             </section>
+
+            <section className='sectionThree'>
+                <img src='https://th.bing.com/th/id/OIP.qqAXLbEpsJTQZ0swSY3osAHaCB?pid=ImgDet&rs=1'/>
+                <img src='https://th.bing.com/th/id/R.ecbe57d4fdedcd8ff984e88d1b4f369e?rik=27h%2buSsSoG6saQ&riu=http%3a%2f%2fwww.freelogovectors.net%2fwp-content%2fuploads%2f2019%2f01%2ftraveloka_logo.png&ehk=lwdKq2zYOaqkUkImbB5zg%2b0lj89HBzUwNeeFTjEcvCQ%3d&risl=&pid=ImgRaw&r=0'/>
+                <img src='https://th.bing.com/th/id/OIP.EsNZbwsk2n5YHrIy-ruihwHaBk?pid=ImgDet&rs=1'/>
+                <img src='https://th.bing.com/th/id/R.2badb0eabba4098b42783009ea4e0fa7?rik=m9%2bC4rfx6RFAyQ&pid=ImgRaw&r=0'/>
+            </section>
+
+            <section className='sectionFour'></section>
         </>
     )
 }
